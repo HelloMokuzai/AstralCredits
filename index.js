05108c49e1e054c5ed6d6c023d6dc9995b84ef13
Jon Dough <49297268+jetstream0@users.noreply.github.com>
2023-03-08 10:12:07 -0800

    Aged songbird requirement added (#5)
    closes #3

--- index.js
+++ index.js
@@ -95,8 +95,14 @@ app.post('/', async function(req, res) {
 	if (!error) {
 		let enough_balance = await songbird.enough_balance(address, HOLDING_REQUIREMENT);
 	  if (!enough_balance) {
-	    error = "Need to hold at least "+String(HOLDING_REQUIREMENT)+" Songbird or Wrapped Songbird to use faucet (this is an anti-botting measure!)";
+	    error = "Need to hold at least "+String(HOLDING_REQUIREMENT)+" Songbird or Wrapped Songbird to use faucet (this is an anti-botting measure, sorry!)";
 	  }
+    if (!error) {
+      let aged_enough = await songbird.aged_enough(address, HOLDING_REQUIREMENT);
+      if (!aged_enough) {
+        error = "Need to have held at least "+String(HOLDING_REQUIREMENT)+" Songbird or Wrapped Songbird for at least 1-2 hours (1800 blocks) before claim. Try to claim again in 1-2 hours (this is an anti-botting measure, sorry!)";
+      }
+    }
 	}
   //if success, try to send
   let amount = String(db.get_amount());
