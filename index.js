e59db9f59c8e59a18f1b6e4c4571f93d593e691e
jetstream0 <49297268+jetstream0@users.noreply.github.com>
2023-04-02 12:50:19 -0700

    allow static cache, fix time bug

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('static'));
+//cache for 2 days
+app.use(express.static('static', { maxAge: 2*24*60*60*1000 }));
 
 nunjucks.configure('templates', { autoescape: true });
 
